66ed3a65ac706c0f2493f5c0bc487d91239633aa
Faisal <alagla.faisal@gmail.com>
2022-10-14 23:17:05 +0300

    test 69

--- App.js
+++ App.js
@@ -1,19 +1,10 @@
-<<<<<<< HEAD
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-=======
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
->>>>>>> fecff7808c338de4de356e088f09eb179b252eb8
 
 export default function App() {
   return (
     <View style={styles.container}>
-<<<<<<< HEAD
-      <Text>Test Test</Text>
-=======
       <Text>Changed last</Text>
->>>>>>> fecff7808c338de4de356e088f09eb179b252eb8
       <StatusBar style="auto" />
     </View>
   );
@@ -22,17 +13,8 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-<<<<<<< HEAD
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
-// test
-=======
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
 });
->>>>>>> fecff7808c338de4de356e088f09eb179b252eb8
