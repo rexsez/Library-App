987bbc5ba3f41b9e350730db67e4be64c5550c35
rexsez <57074750+rexsez@users.noreply.github.com>
2022-11-10 22:11:56 +0300

    Bardcode scanner first functional version

--- components/SearchScreenComponents/BarcodeScanner.js
+++ components/SearchScreenComponents/BarcodeScanner.js
@@ -3,20 +3,69 @@ import React, { useState, useEffect } from "react";
 import { BarCodeScanner } from "expo-barcode-scanner";
 
 function BarcodeScanner() {
-  const [hasPermission, setHasPermission] = useState();
+  const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [text, setText] = useState("Not yet scanned");
 
   const askForCameraPermission = () => {
-    async () => {
+    (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
       setHasPermission(status == "granted");
-    };
+    })();
   };
 
+  // Request Camera Permission
+
+  useEffect(() => {
+    askForCameraPermission();
+  }, []);
+
+  // What happens when we scan the bar code
+  const handleBardCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    setText(data);
+    console.log("Type: " + type + "\nData: " + data);
+  };
+
+  // Check permission and return the screens
+
+  if (hasPermission === null) {
+    return (
+      <View style={styles.container}>
+        <Text>Requesting for Camera Permission</Text>
+      </View>
+    );
+  }
+
+  if (hasPermission === false) {
+    return (
+      <View style={styles.container}>
+        <Text style={{ margin: 10 }}>No access to camera</Text>
+        <Text style={{ margin: 10 }}>
+          Please Allow Access to camera from the Settings
+        </Text>
+      </View>
+    );
+  }
+
+  // Return the view
   return (
     <View style={styles.container}>
-      <Text>Hi this is barcode scanner</Text>
+      <View style={styles.barcodeContainer}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBardCodeScanned}
+          style={{ height: 400, width: 400 }}
+        />
+      </View>
+
+      <Text style={styles.maintext}> {text}</Text>
+      {scanned && (
+        <Button
+          title="Scan again"
+          onPress={() => setScanned(false)}
+          color="tomato"
+        />
+      )}
     </View>
   );
 }
@@ -24,5 +73,24 @@ function BarcodeScanner() {
 export default BarcodeScanner;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: "#fff",
+  },
+  barcodeContainer: {
+    alignItems: "center",
+    justifyContent: "center",
+    height: 300,
+    width: 300,
+    overflow: "hidden",
+    borderRadius: 30,
+    backgroundColor: "tomato",
+  },
+
+  maintext: {
+    fontSize: 16,
+    margin: 20,
+  },
 });
