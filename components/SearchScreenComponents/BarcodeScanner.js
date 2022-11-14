e660e3b0ebf54a69c8a10d3b3eae91a9392d12f7
rexsez <57074750+rexsez@users.noreply.github.com>
2022-11-14 22:10:17 +0300

    Added some error handling to the barcode scanner, now android will go back to search screen

--- components/SearchScreenComponents/BarcodeScanner.js
+++ components/SearchScreenComponents/BarcodeScanner.js
@@ -1,4 +1,4 @@
-import { View, Text, StyleSheet, Button, Platform } from "react-native";
+import { View, Text, StyleSheet, Button, Platform, Alert } from "react-native";
 import React, { useState, useEffect } from "react";
 import { BarCodeScanner } from "expo-barcode-scanner";
 
@@ -20,20 +20,52 @@ function BarcodeScanner({ navigation }) {
     })();
   };
 
+  // To check if input is valid, ISBN shouldn't contain any letter ot special character
+  function containsOnlyNumbers(str) {
+    return /^\d+$/.test(str);
+  }
+
   // Request Camera Permission
   useEffect(() => {
     askForCameraPermission();
   }, []);
 
   // If something is scanned we will update the states declared above
-  const handleBardCodeScanned = ({ type, data }) => {
+  const handleBardCodeScanned = ({ data }) => {
+    // This is needed, because if something is scanned, I need to show scan again button if the platform is IOS
     setScanned(true);
-    setText(data);
-    const selectedBook = BOOKS.find((book) => book.isbn === +data);
-
-    if (selectedBook)
-      navigation.navigate("Book", { bookId: data, isScanned: true });
-    else navigation.navigate("Add", { bookId: data, isScanned: true });
+    // ISBN have 13 or 10 digit number (according to my quick googling), so I must ensure that
+    // the scanned barcode is 10 or 13 number, and doesn't include anything but numbers
+    if (
+      (data.length === 13 || data.length === 10) &&
+      containsOnlyNumbers(data)
+    ) {
+      setText(data);
+      // Checking if the list of books we have include the scanned barcode, which is needed to decide
+      // which page to load, if we have I'll load the book information page. Otherwise, Add book screen should
+      // be loaded to give the option to user to add the new book
+      // Parse int is needed because we stored the isbn as a number, while the scanner is reading
+      // it as a string
+      const selectedBook = BOOKS.find((book) => book.isbn === parseInt(data));
+      //  If selectedBook is set (not null),it means that there is a book and we should go to book information
+      if (selectedBook)
+        navigation.navigate("Book", { bookId: data, isScanned: true });
+      else navigation.navigate("Add", { bookId: data, isScanned: true });
+      // selectedBook not set means there is no book with the scanned ISBN, so we should load Add book screen
+    } else {
+      setText("Not yet scanned");
+      Alert.alert("Invalid Barcode!", "Please Scan Again", [
+        {
+          text: "Okey",
+          onPress: () =>
+            // only android will be redirected to Search screen page (Bug in the framework), IOS won't be redirected
+            Platform.OS === "android"
+              ? navigation.navigate("SearchScreenStack")
+              : undefined,
+          style: "cancel",
+        },
+      ]);
+    }
   };
 
   // Check permission and return the screens
@@ -63,7 +95,7 @@ function BarcodeScanner({ navigation }) {
       <View style={styles.barcodeContainer}>
         <BarCodeScanner
           onBarCodeScanned={scanned ? undefined : handleBardCodeScanned}
-          style={{ height: 400, width: 400 }}
+          style={{ height: 500, width: 500 }}
         />
       </View>
 
@@ -95,7 +127,6 @@ const styles = StyleSheet.create({
     width: 300,
     overflow: "hidden",
     borderRadius: 30,
-    backgroundColor: "tomato",
   },
 
   maintext: {
